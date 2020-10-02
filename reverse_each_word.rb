1c4e81e587a2fa4c1b9948ece251f9950b53cf7d
Naga Bhargavi Marudhuri <bhargavimarudhuri@gmail.com>
2020-10-02 04:01:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,3 @@
 def reverse_each_word(myStr)
-  result.split(" ").collect { |x| x.reverse }.reduce{ |result, str| result + " " + str }
+  myStr.split(" ").collect { |x| x.reverse }.reduce{ |result, str| result + " " + str }
 end
